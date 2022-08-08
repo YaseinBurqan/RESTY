f520acd4f9eb30fccb759f62b51ac5fd1bfbc05d
Yasein Burqan <mr.yasein@gmail.com>
2022-08-08 11:06:31 +0300

    create react app

---
+++ client/src/App.js
@@ -0,0 +1,21 @@
+import "./App.css";
+import React from "react";
+
+// function App() {
+// return(
+class App extends React.Component {
+  return() {
+    <>
+      <div className="App">
+        <header className="App-header">
+          <p>
+            <code>Welcome in my app </code>
+          </p>
+        </header>
+      </div>
+    </>;
+  }
+  // )
+}
+
+export default App;
