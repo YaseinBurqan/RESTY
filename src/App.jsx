42ff41b223fb8c89690bb676c35b835737d54922
Yasein Burqan <mr.yasein@gmail.com>
2022-08-16 22:32:00 +0300

    use effect

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,90 @@
 import "./App.scss";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 import Header from "./components/header/header.jsx";
 import Footer from "./components/footer/footer.jsx";
 import Form from "./components/form/form";
 import Results from "./components/results/results";
 
+import axios from "axios";
+
 export default function App() {
   const [data, setData] = useState();
   const [resultData, setResultData] = useState("");
 
+  useEffect(() => {
+    if (resultData.method === "get") {
+      axios
+        .get(resultData.url)
+        .then((data) => {
+          const dataDetails = {
+            header: data.headers,
+            data: data.data,
+            count: data.data.length,
+          };
+          setData(dataDetails);
+        })
+        .catch((error) => {
+          console.log(error);
+          setData("Error");
+        });
+    }
+
+    if (resultData.method === "post") {
+      axios
+        .post(resultData.url, resultData.body)
+        .then(function (data) {
+          const dataDetails = {
+            header: data.headers,
+            data: data.data,
+            count: data.data.length,
+          };
+          setData(dataDetails);
+        })
+        .catch((error) => {
+          console.log(error);
+          setData("Error");
+        });
+    }
+
+    if (resultData.method === "put") {
+      axios
+        .put(resultData.url, resultData.body)
+        .then((data) => {
+          const dataDetails = {
+            header: data.headers,
+            data: data.data,
+            count: data.data.length,
+          };
+          setData(dataDetails);
+        })
+        .catch((error) => {
+          console.log(error);
+          setData("Error");
+        });
+    }
+
+    if (resultData.method === "delete") {
+      axios
+        .delete(resultData.url)
+        .then((data) => {
+          const dataDetails = {
+            header: data.headers,
+            data: data.data,
+            count: data.data.length,
+          };
+          setData(dataDetails);
+        })
+        .catch((error) => {
+          console.log(error);
+          setData("Error");
+        });
+    }
+  }, [resultData]);
+
   const callApi = (requestParams) => {
-    const data = {
-      count: 2,
-      results: [
-        { name: "fake thing 1", url: "http://fakethings.com/1" },
-        { name: "fake thing 2", url: "http://fakethings.com/2" },
-      ],
-    };
-    setResultData();
-    setData(data);
+    setResultData(requestParams);
+    setData({});
   };
 
   return (
--- src/components/form/form.jsx
+++ src/components/form/form.jsx
@@ -2,30 +2,77 @@ import React, { useState } from "react";
 import "./form.scss";
 
 function Form(props) {
+  const [apiUrl, setUrl] = useState("https://pokeapi.co/api/v2/pokemon");
+  const [method, setMethod] = useState("");
+  const [body, setBody] = useState("");
+
   const handleSubmit = (e) => {
     e.preventDefault();
     const formData = {
-      method: "GET",
-      url: "https://pokeapi.co/api/v2/pokemon",
+      url: apiUrl,
+      method: method,
+      body: body,
     };
     props.handleApiCall(formData);
+  };
+
+  const inputHandler = (element) => {
+    console.log("url= ", document.getElementById("url").value);
+    setUrl(document.getElementById("url").value);
+  };
+
+  const handleBody = (element) => {
+    console.log(document.getElementById("body").value);
+    setBody(document.getElementById("body").value);
   };
 
   return (
     <>
       <form onSubmit={handleSubmit}>
         <label>
-          <span>URL: </span>
-          <input name="url" type="text" />
-          <button type="submit">GO!</button>
+          <span>URL </span>
+          <input id="url" name="url" type="text" onInput={inputHandler} data="url" />
+          <button type="submit" data="go">
+            GO!
+          </button>
         </label>
+
         <label className="methods">
-          <span id="get">GET</span>
-          <span id="post">POST</span>
-          <span id="put">PUT</span>
-          <span id="delete">DELETE</span>
+          <span
+            id="get"
+            onClick={() => {
+              setMethod("get");
+            }}
+          >
+            GET
+          </span>
+          <span
+            id="post"
+            onClick={() => {
+              setMethod("post");
+            }}
+          >
+            POST
+          </span>
+          <span
+            id="put"
+            onClick={() => {
+              setMethod("put");
+            }}
+          >
+            PUT
+          </span>
+          <span
+            id="delete"
+            onClick={() => {
+              setMethod("delete");
+            }}
+          >
+            DELETE
+          </span>
         </label>
       </form>
+      {method === "post" || method === "put" ? <textarea id="body" onInput={handleBody} placeholder="name=body"></textarea> : null}
     </>
   );
 }
